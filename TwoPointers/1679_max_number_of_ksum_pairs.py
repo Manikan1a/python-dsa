ebb9fbead8a56642c0d8dbd1c127b20d84f3b4fc
Manikan1a <saimanikantareddy967@gmail.com>
2026-08-11 18:54:11 +0530

    Max K-Sum Pairs

---
+++ TwoPointers/1679_max_number_of_ksum_pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left=0
+        right=len(nums)-1
+        max_oper=0
+        while left<right:
+            if nums[left]+nums[right]==k:
+                max_oper+=1
+                left+=1
+                right-=1
+            elif nums[left]+nums[right]<k:
+                left+=1
+            elif nums[left]+nums[right]>k:
+                right-=1
+        return max_oper
